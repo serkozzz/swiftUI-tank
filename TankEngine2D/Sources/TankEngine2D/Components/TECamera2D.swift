5c0eb411e3e4ef388a5f28d2c0269715e6080232
Sergey Kozlov <s.kozlov@blackmana.com>
2025-10-10 08:13:30 +0300

    comments

--- TankEngine2D/Sources/TankEngine2D/Components/TECamera2D.swift
+++ TankEngine2D/Sources/TankEngine2D/Components/TECamera2D.swift
@@ -7,6 +7,11 @@
 import simd
 import SwiftUI
 
+
+/// Coordinate system details:
+/// - The renderer is flipped along the Y axis (origin at bottom-left), while gesture inputs
+///   typically come from a non-flipped UIKit/SwiftUI coordinate space (origin at top-left).
+///   `screenToWorld(_:)` compensates by inverting Y using the current `viewportSize`.
 public class TECamera2D: TEComponent2D  {
     
     @Published public var viewportSize: CGSize = .zero
@@ -15,9 +20,13 @@ public class TECamera2D: TEComponent2D  {
         transform?.move(vector)
     }
     
+    ///
     public func screenToWorld(_ point: SIMD2<Float>) -> SIMD2<Float> {
         guard let transform else { printNotAttachedError(); fatalError();}
-        
+        //we invert Y in screenToWorld because user will hang .gesture on unflipped View outside the sceneRenderer.
+        //The same time SceneREnderer2D flipped by Oy.
+        //We don't need to do it in worldToScreen because worldToScreen usualy works inside engine ecosystem
+        //where we work with already flipped coords only.
         let invertedY = SIMD2<Float>(point.x, Float(viewportSize.height) - point.y)
         let cameraSpace = invertedY - SIMD2<Float>(cgSize: viewportSize) / 2
         let cameraSpaceHomogeneous = SIMD3<Float>(cameraSpace, 1)
